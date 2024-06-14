fb15ccc7a8ff02e602553db6c3525952afb44a32
Fernando Jara <fjaraw@hotmail.com>
2024-06-13 21:05:07 -0400

    Mejoras en alertas

--- main/views.py
+++ main/views.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse
 from main.forms import ContactForm
 from main.models import Contact, Flan
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView
+
+class LoginViewPropia(SuccessMessageMixin, LoginView):
+    success_message = "Has ingresado correctamente"
 
 # Create your views here.
 def index(req):
--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from main.views import LoginViewPropia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', LoginViewPropia.as_view()),
     path('accounts/', include('django.contrib.auth.urls')),
     path('',include('main.urls')),
     #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
